a97795f48e79f9494efe2014951b37cc3f0b4e8e
Ari <82783339+Arihany@users.noreply.github.com>
2026-01-30 02:09:56 +0900

    Update Toon11.kt

--- src/ko/toon11/src/eu/kanade/tachiyomi/extension/ko/toon11/Toon11.kt
+++ src/ko/toon11/src/eu/kanade/tachiyomi/extension/ko/toon11/Toon11.kt
@@ -24,7 +24,6 @@ import rx.Observable
 import java.io.IOException
 import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.ArrayList
 import java.util.Date
 import java.util.Locale
 
